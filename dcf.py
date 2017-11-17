59ee38d87310f6964e910d5c840b178a1d9b4e36
jetbennett <wolleytej@gmail.com>
2017-11-17 16:16:28 -0500

    Add files via upload

--- dcf.py
+++ dcf.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 '''
-Grady Denton & Shane Bennet for proj2 in cnt5505 data comm
+Grady Denton & Shane Bennett for proj3 in cnt5505 data comm
 '''
+
+#***Check to make sure we need all these***
 from __future__ import division
 import argparse
 import os
 from collections import deque
+from copy import deepcopy
+from random import *
 
-# Function definitions
+#*** Function definitions ***
 def processQue(packet_queu, time, numSuccess):
   while(packet_queu and (int(packet_queu[0][4]) + int(packet_queu[0][3]) <= time or time == -1)):
     if packet_queu[0][5] == ": collision":
@@ -18,15 +22,27 @@ def processQue(packet_queu, time, numSuccess):
     of.write("Time: {} Packet: {}: {} {} {} {} {}\n".format(int(packet_queu[0][4]) + int(packet_queu[0][3]), packet_queu[0][0], packet_queu[0][1], packet_queu[0][2], packet_queu[0][3], packet_queu[0][4], finish))
     packet_queu.popleft()
   return numSuccess
-
-
-# set up arguments
-parser = argparse.ArgumentParser(prog='aloha', description='Simulates the aloha protocol for a given traffic file.')
+  
+#returns a time 0 to 15 at first
+#numBackOffs increments when an ack is not received
+def binExpBackoff(numOfBackoffs, slotTime, totalLatencyPerNode, src_node):
+	if(numOfBackoffs = 0)
+		timeToWait = randint(0, 15*slotTime)
+	elif(numOfBackoffs >=6)
+		timeToWait = randint(0, 1024*slotTime)
+	else
+		timeToWait = randint(0, (32 * (2 ** (numOfBackoffs-1))) * slotTime)
+		#!!!!Will this actually be changed!?!?
+	totalLatencyPerNode[src_node] = totalLatencyPerNode[src_node] + timeToWait
+  return timeToWait
+  
+#*** set up arguments ***
+parser = argparse.ArgumentParser(prog='DCF', description='Simulates the 802.11 DCF MAC protocol for a given traffic file.')
 parser.add_argument("-t","--trafficfile", help="traffic file", default=os.path.join(os.getcwd(), "traffic"))
-parser.add_argument("-o","--outfile", help="output file", default=os.path.join(os.getcwd(), "aloha.out"))
+parser.add_argument("-o","--outfile", help="output file", default=os.path.join(os.getcwd(), "DCF.out"))
 args = parser.parse_args()
 
-# check arguments
+#*** check arguments ***
 trafficfile = os.path.expanduser(args.trafficfile)
 if not os.path.exists(trafficfile):
   parser.error('The trafficfile file does not exist!')
@@ -46,48 +62,106 @@ if not os.path.isdir(outDir):
 if not os.access(outDir, os.W_OK):
   parser.error('The outDir dir is not writable!')
 
-# MAIN FUNCTION
+#****** MAIN FUNCTION ******
 
-# Vars
-packet_queu = deque()
-numSuccess = 0
+#*** Vars ***
+sending_qwee = deque()
+waiting_qwee = deque()
 outPath = outDir + "/" + outfile
 
+numOfCollisions = 0
+timeMediaUtilized = 0	#increment this by (pkt_size/dataRate) when a packet successfully sends
+totalTime = 0 #set this equal to the time the last packet successfully finishes transmission
+numOfTransmissions = 0 #increment each time a packet attempts transmission
+numPktPerNode = []
+totalLatencyPerNode = [] #increment this by "wait time" whenever a packet must wait to send
+
+dataRate = 6	#Mbps, 6 bits are sent per microsecond
+ackTime = 44	#us
+slotTime = 9	#us
+difsTime = 28	#us
+sifsTime = 10	#us
+	
 # do stuff
 with open(outPath, 'w') as of:
   with open(trafficfile, 'r') as tf:
     stats = tf.readline()
     stats = stats.split()
     numPackets = int(stats[0])
-    offerdLoad = float(stats[1])
+    #offerdLoad = float(stats[1])
 
     for line in tf:
+	  #**** line format *****
+	  #packet[0] : pkt_id   *
+	  #packet[1] : src_node *
+	  #packet[2] : dst_node *
+	  #packet[3] : pkt_size *
+	  #packet[4] : time     *
+	  #**********************
+		
       packet = line.split()
       time = int(packet[4])
       packet.append("")
+	  
+		#increase list sizes if necessary
+			while(len(numPktPerNode) < packet[1] + 1)
+				numPktPerNode.append(0)
+				totalLatencyPerNode.append(0)
+				
+			#count how many packets each node sends
+			++numPktPerNode[packet[1]]
+	
+	  
+      #wait DIFS time
+			time = time + difsTime
+			
+			#process what should have happened
+      numSuccess = processQue(sending_qwee, time, numOfTransmissions, numOfCollisions, timeMediaUtilized)
 
-      #print finished packets
-      numSuccess = processQue(packet_queu, time, numSuccess)
+      # ***** Changed for csma
+      # copy waiting queu to sending queu
+      if not packet_queu and waiting_queu:
+        packet_queu = deepcopy(waiting_queu)
+        waiting_queu.clear()
+        if len(packet_queu) > 1:
+          for p in packet_queu:
+            #print packet sending message
+            of.write("Time: {} Packet: {}: {} {} {} {} start sending{}\n".format(p[4], p[0], p[1], p[2], p[3], p[4], p[5]))
+            p[5] = ": collision"
+        elif len(packet_queu) == 1:
+          for p in packet_queu:
+            #print packet sending message
+            of.write("Time: {} Packet: {}: {} {} {} {} start sending{}\n".format(p[4], p[0], p[1], p[2], p[3], p[4], p[5]))
+        numSuccess = processQue(packet_queu, time, numSuccess)
 
-      #check for collisions
+      # processing current packet
       if packet_queu:
-        packet[5] = ": collision"
-        if len(packet_queu) == 1:
-          packet_queu[0][5] = ": collision"
-
-      #print packet sending message
-      of.write("Time: {} Packet: {}: {} {} {} {} start sending{}\n".format(packet[4], packet[0], packet[1], packet[2], packet[3], packet[4], packet[5]))
-
-      packet_queu.append(packet)
+        packet[4] = int(packet_queu[0][4]) + packetSize
+        waiting_queu.append(packet)
+      else:
+        packet_queu.append(packet)
+      # ***** end changed for csma
 
     #finish off the queu
-    packetSize = int(packet_queu[-1][3])
-    timeLastPacketFinished = int(packet_queu[-1][4]) + packetSize
+    if waiting_queu:
+        timeLastPacketFinished = int(waiting_queu[-1][4]) + packetSize
+    else:
+        timeLastPacketFinished = int(packet_queu[-1][4]) + packetSize
     numSuccess = processQue(packet_queu, -1, numSuccess)
+    if len(waiting_queu) > 1:
+        for p in waiting_queu:
+          #print packet sending message
+          of.write("Time: {} Packet: {}: {} {} {} {} start sending{}\n".format(p[4], p[0], p[1], p[2], p[3], p[4], p[5]))
+          p[5] = ": collision"
+    numSuccess = processQue(waiting_queu, -1, numSuccess)
 
     of.write("Packet Size: {}  NumSuccess: {}  TimeLastPacketFinished: {} \n".format(packetSize, numSuccess, timeLastPacketFinished))
 
 statfile = outDir + "/" + outfile + ".stats"
-throughput = (numSuccess * packetSize) / timeLastPacketFinished
+
+#**** Output some statistics here ****
+#throughput = (timeMediaUtilized * dataRate) / totalTime
+#fracMediaFree = (totalTime - timeMediaUtilized) / totalTime
+#avgLatencyPerNode = totalLatencyPerNode[i] / numPktPerNode[i]
 with open(statfile, 'w') as sf:
   sf.write("{},{}\n".format(offerdLoad,throughput))
